a54b818a23c08e733984d669f962ec5056bfdc2a
Nicolas <nicolas.tsan@gmail.com>
2017-08-28 18:44:27 +0200

    slide ok

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,11 @@
-$(function(){
-   alert('jQuery est prêt !');
+$(document).ready(function(){
+
+	var t = setInterval(function(){
+		$("#carousel ul").animate({marginLeft:-940},1000,function(){
+			$(this).find("li:last").after($(this).find("li:first"));
+			$(this).css({marginLeft:0});
+
+		})
+	},4000); 
 });
+
